2f6ef15de9a8c51b471a379c7746f7ed4a72ec7a
siyam79 <siyamahmed3827@gmail.com>
2023-10-21 16:46:51 +0600

    my card section emplements thne.   user add too cart products .. and products store my card raoute

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     return (
         <section id="contact" className=" py-10 px-3 text-white bg-slate-900 ">
             <div className=" text-center mt-8 ">
-                <h3 className="text-2xl font-semibold"> Contact <span className=" text-pink-600">Me</span>  </h3>
+                <h3 className="text-2xl font-semibold"> Contacts <span className=" text-pink-600">Me</span>  </h3>
                 <p className="text-pink-600 mt-3 text-lg ">Get In Touch</p>
                 <div className=" mt-16 flex md:flex-row flex-col gap-6 mx-w-5xl bg-slate-900 md:p-6 p-2  rounded-lg mx-auto  ">
                     <form className=" flex flex-col flex-1 gap-5 ">
